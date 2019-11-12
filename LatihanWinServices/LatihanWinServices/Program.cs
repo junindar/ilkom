2625cf5e26ebc79bd77684889e84c59a1300aaa4
junindar <junindar@gmail.com>
2019-11-12 09:56:59 +0700

    First Project

---
+++ LatihanWinServices/LatihanWinServices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LatihanWinServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
