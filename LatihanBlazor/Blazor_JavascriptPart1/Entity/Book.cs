9688d5231d6b16acbb7c2664512d3134ed41cc02
Junindar <junindar@gmail.com>
2021-08-31 12:11:59 +0700

    Javascript Interop Part 1

---
+++ LatihanBlazor/Blazor_JavascriptPart1/Entity/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_JavascriptPart1.Entity
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Judul { get; set; }
+        public string Penulis { get; set; }
+        public string Penerbit { get; set; }
+        public string Deskripsi { get; set; }
+        public bool Status { get; set; }
+        public string Gambar { get; set; }
+
+    }
+   
+}
---
+++ LatihanBlazor/Blazor_JavascriptPart1/wwwroot/scripts/blazorInterop.js
@@ -0,0 +1,25 @@
+﻿var blazorInterop = blazorInterop || {};
+
+blazorInterop.showAlert=function(message) {
+    alert(message);
+};
+
+
+
+blazorInterop.showPrompt = function (message,defaultValue) {
+    return prompt(message, defaultValue);
+};
+
+blazorInterop.createBook = function (judul, penulis) {
+    let book = 
+        {
+            judul: judul,
+            penulis: penulis,
+            penerbit: "Cahaya Buku"
+            
+        };
+    return book;
+};
+
+
+
