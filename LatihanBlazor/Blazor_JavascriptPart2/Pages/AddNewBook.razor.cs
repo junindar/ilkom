a0314559573fb026302d579839508ea96f2a6e59
Junindar <junindar@gmail.com>
2021-09-05 11:43:32 +0700

    Invoke JS Part 2

---
+++ LatihanBlazor/Blazor_JavascriptPart2/Pages/AddNewBook.razor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazor_JavascriptPart2.Entity;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Blazor_JavascriptPart2.Pages
+{
+    public partial class AddNewBook 
+    {
+        [Inject] public IJSRuntime JsRuntime { get; set; }
+        public Book Book { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(0);
+            Book = new Book();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JsRuntime.InvokeVoidAsync("blazorInterop.focusElementById", "Judul");
+            }
+
+        }
+    }
+}
---
+++ LatihanBlazor/Blazor_JavascriptPart2/wwwroot/scripts/blazorInterop.js
@@ -0,0 +1,19 @@
+﻿var blazorInterop = blazorInterop || {};
+
+blazorInterop.showAlert = function (message) {
+    alert(message);
+};
+
+blazorInterop.focusElement = function (element) {
+    element.focus();
+};
+
+
+blazorInterop.focusElementById = function (id) {
+    var element = document.getElementById(id);
+    if (element) element.focus();
+};
+
+blazorInterop.throwsError = function () {
+    throw Error("Error from JS Function");
+}
