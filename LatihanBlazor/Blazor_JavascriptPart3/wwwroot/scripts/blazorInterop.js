3688fb6cd79ba697b1b51d7ff88a6be20c1e484d
Junindar <junindar@gmail.com>
2021-10-04 07:20:20 +0700

    invoke JS part 3

---
+++ LatihanBlazor/Blazor_JavascriptPart3/wwwroot/scripts/blazorInterop.js
@@ -0,0 +1,56 @@
+﻿var blazorInterop = blazorInterop || {};
+
+
+blazorInterop.methodCalculate = function () {
+    var promise = DotNet.invokeMethodAsync("Blazor_JavascriptPart3",
+        "Calculate", 5);
+    promise.then(result => alert(result));
+};
+
+blazorInterop.methodCalculateCustomIdentifier = function () {
+    var promise = DotNet.invokeMethodAsync("Blazor_JavascriptPart3",
+        "CalculateWithVal2", 5, 6);
+    promise.then(result => alert(result));
+};
+
+
+blazorInterop.sayHello1 = (dotNetHelper) => {
+    return dotNetHelper.invokeMethodAsync('GetHelloMessage');
+};
+
+blazorInterop.callDotNetSetWindowSizeMethod = function (dotNetObjectRef) {
+    dotNetObjectRef.invokeMethodAsync("SetWindowSize",
+        {
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+};
+
+
+blazorInterop.registerResizeHandler = function (dotNetObjectRef) {
+    function resizeHandler() {
+        dotNetObjectRef.invokeMethodAsync("SetWindowSize",
+            {
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
+    };
+
+    // Set up initial values
+    resizeHandler();
+
+    // Register event handler
+    window.addEventListener("resize", resizeHandler);
+};
+
+
+
+
+
+blazorInterop.startRandomGenerator = function (dotNetObject) {
+    setInterval(function () {
+        let text = Math.random() * 1000;
+        console.log("JS: Generated " + text);
+        dotNetObject.invokeMethodAsync('AddText', text.toString());
+    }, 1000);
+};
---
+++ LatihanBlazor/Blazor_JavascriptPart3/wwwroot/scripts/browserInterop.js
@@ -0,0 +1,36 @@
+﻿var jsObjectRefs = {};
+var jsObjectRefId = 0;
+const jsRefKey = '__jsObjectRefId';
+DotNet.attachReviver(function (key, value) {
+    if (value &&
+        typeof value === 'object' &&
+        value.hasOwnProperty(jsRefKey) &&
+        typeof value[jsRefKey] === 'number') {
+
+        var id = value[jsRefKey];
+        if (!(id in jsObjectRefs)) {
+            throw new Error("This JS object reference does not exists : " + id);
+        }
+        const instance = jsObjectRefs[id];
+        return instance;
+    } else {
+        return value;
+    }
+});
+function cleanObjectRef(id) {
+    console.log(jsObjectRefId);
+    delete jsObjectRefs[jsObjectRefId];
+}
+function storeObjectRef(obj) {
+    var id = jsObjectRefId++;
+    jsObjectRefs[id] = obj;
+    var jsRef = {};
+    jsRef[jsRefKey] = id;
+    return jsRef;
+}
+function openWindow() {
+    return storeObjectRef(window.open("/", "_blank"));
+}
+function closeWindow(window) {
+    window.close();
+}
