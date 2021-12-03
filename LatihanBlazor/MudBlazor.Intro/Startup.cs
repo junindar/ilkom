98290830869d713af64bbc3ff99d4f546a1f7936
Junindar <junindar@gmail.com>
2021-12-03 18:24:57 +0700

    MudBlazor

--- LatihanBlazor/MudBlazor.Intro/Startup.cs
+++ LatihanBlazor/MudBlazor.Intro/Startup.cs
@@ -31,6 +31,7 @@ namespace MudBlazor.Intro
             services.AddServerSideBlazor();
             services.AddMudServices();
             services.AddSingleton<WeatherForecastService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
