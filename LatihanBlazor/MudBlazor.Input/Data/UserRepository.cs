c1286f19ac595047da0a9d581047b411c9dc9eda
Junindar <junindar@gmail.com>
2022-09-07 14:22:09 +0700

    Input Component

---
+++ LatihanBlazor/MudBlazor.Input/Data/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MudBlazor.Input.Data.ViewModel;
+using Newtonsoft.Json;
+
+namespace MudBlazor.Input.Data
+{
+    public class UserRepository : IUserRepository
+    {
+
+        public HttpClient _httpClient { get; }
+      
+        public UserRepository(HttpClient httpClient)
+        {
+ 
+
+            httpClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            _httpClient = httpClient;
+        }
+
+      
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "users");
+            var response = await _httpClient.SendAsync(requestMessage);
+
+
+            var responseStatusCode = response.StatusCode;
+
+            if (responseStatusCode.ToString() == "OK")
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return await Task.FromResult(JsonConvert.DeserializeObject<List<User>>(responseBody));
+            }
+
+            return null;
+        }
+
+      
+
+
+    }
+}
---
+++ LatihanBlazor/MudBlazor.Input/Data/ViewModel/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MudBlazor.Input.Data.ViewModel
+{
+    public class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
